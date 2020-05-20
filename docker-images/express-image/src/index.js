d1aa87531ea5c5d55d5d1d33c6c1ce1be520ec43
vitorva <vitor.vazafonso@gmail.com>
2020-05-20 15:57:17 +0200

    generate random number of professions and return JSON payload

--- docker-images/express-image/src/index.js
+++ docker-images/express-image/src/index.js
@@ -5,9 +5,25 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req,res) {
-	res.send( chance.profession() );
+	res.send( generateProfessions() );
 });
 
 app.listen(3000, function () {
 	console.log('Accepting HTTP requets on port 3000');
 });
+
+function generateProfessions() {
+	var numberOfProfessions = chance.integer({
+		min: 0,
+		max: 10
+	});
+	console.log(numberOfProfessions);
+	var professions = [];
+	for (var i = 0; i< numberOfProfessions; i++){
+		professions.push({
+			profession : chance.profession()
+		});
+	};
+	console.log(professions);
+	return professions;
+}
