2eacaaeb98c2941d791931ab05a3157615016a6f
vitorva <vitor.vazafonso@gmail.com>
2020-05-20 15:28:14 +0200

    return different content than the webcast

--- docker-images/express-image/src/index.js
+++ docker-images/express-image/src/index.js
@@ -5,37 +5,9 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req,res) {
-	res.send( generateStudents() );
+	res.send( chance.profession() );
 });
 
 app.listen(3000, function () {
 	console.log('Accepting HTTP requets on port 3000');
 });
-
-function generateStudents() {
-	var numberOfStudents = chance.integer({
-		min: 0,
-		max: 10
-	});
-	console.log(numberOfStudents);
-	var students = [];
-	for (var i = 0; i< numberOfStudents; i++){
-		var gender = chance.gender();
-		var birthday = chance.year({
-			min: 1990,
-			max: 2000
-		});
-		students.push({
-			firstName : chance.first({
-				gender: gender
-			}),
-			lastName: chance.last(),
-			gender: gender,
-			birthday: chance.birthday({
-				year : birthday
-			})
-		});
-	};
-	console.log(students);
-	return students;
-}
